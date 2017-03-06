dcd36b8b8084ac9062e10614f1e91746dddb0d38
John Quinn <john.quinn@ucd.ie>
2017-03-06 08:48:59 +0000

    added counter and logging (just in case)

--- RunStamp/RunStamp.py
+++ RunStamp/RunStamp.py
@@ -5,15 +5,26 @@ import ui
 import clipboard
 import console
 
+import logging
 import datetime
 import time
 
-global results
+logging.basicConfig(filename='RunStamp.log',level=logging.INFO)
+
+#global results
 results=""
+so_far=0
 
 def get_time():
 	return datetime.datetime.strftime(datetime.datetime.now(), '%H:%M:%S')
-
+	
+def update_results(no, id, DNF=False):
+	global results
+	DNFstr='DNF' if DNF else ''
+	tmp="{:3d} {:} {:>4s} {:}\n".format(no,get_time(),id, DNFstr)
+	logging.info(tmp)
+	results=tmp+results
+	
 
 @ui.in_background
 def update_time(sender):
@@ -26,6 +37,7 @@ def button_tapped(sender):
 	# Get the button's title for the following logic:
 	t = sender.title
 	global results
+	global so_far
 	# Get the labels:
 	label = sender.superview['runnerID']
 	resultstxt = sender.superview['resultstxt']
@@ -40,16 +52,11 @@ def button_tapped(sender):
 		label.text = label.text[:-1]
 		if len(label.text) == 0:
 			label.text = ''
-	elif t == 'DNF':
+	elif t == 'DNF' or t == 'Stamp':
 		if len(label.text)>0:
-			tmp="{:} {:>5s} DNF\n".format(get_time(),label.text)
-			results=tmp+results
-			resultstxt.text=results
-			label.text=""
-	elif t == 'Stamp':
-		if len(label.text)>0:
-			tmp="{:} {:>5s}\n".format(get_time(),label.text)
-			results=tmp+results
+			DNF=True if t == 'DNF' else False
+			so_far+=1
+			update_results(so_far, label.text,DNF)
 			resultstxt.text=results
 			label.text=""
 	elif t == 'Copy':
@@ -58,6 +65,7 @@ def button_tapped(sender):
 	elif t == 'Reset':
 		res=console.alert('Are you sure?', 'Clear Results','Proceed')
 		results=''
+		so_far=0
 		resultstxt.text=results
 
 v = ui.load_view()
