830047441ea9d1023149ea638d3adf2a7fd6f09e
John Quinn <john.quinn@ucd.ie>
2017-03-13 11:00:05 +0000

    updated to allow marking of mistakes and correct tracking of number of runners

--- RunStamp/RunStamp.py
+++ RunStamp/RunStamp.py
@@ -15,22 +15,21 @@ logging.basicConfig(filename='RunStamp.log',level=logging.INFO)
 #global results
 results=''
 results_csv=''
-so_far=0
+so_far=[]
 
 def get_time():
 	return datetime.datetime.strftime(datetime.datetime.now(), '%H:%M:%S')
 	
 	
-def update_results(no, id, DNF=False):
+def update_results(no, id, MSG=''):
 	global results
 	global results_csv
 	global runners
 	
-	DNF_str='DNF' if DNF else ''
 	name=runners.get(id)
 	name=name if name else ''
-	tmp="{:3d} {:} {:>4s} {:} {:}\n".format(no,get_time(),id, name, DNF_str)
-	tmp_csv="{:3d}, {:}, {:>4s}, {:}, {:}\n".format(no,get_time(),id, name, DNF_str)
+	tmp="{:3d} {:} {:>4s} {:} {:}\n".format(no,get_time(),id, name, MSG)
+	tmp_csv="{:3d}, {:}, {:>4s}, {:}, {:}\n".format(no,get_time(),id, name, MSG)
 	logging.info(tmp_csv)
 	results=tmp+results
 	results_csv=tmp_csv+results_csv
@@ -46,15 +45,22 @@ def update_time(sender):
 def button_tapped(sender):
 	# Get the button's title for the following logic:
 	t = sender.title
+	
 	global results
 	global results_csv
 	global so_far
+	
 	# Get the labels:
 	label = sender.superview['runnerID']
-	resultstxt = sender.superview['resultstxt']
+	
+	resultstxt =sender.superview['resultstxt']
+	
+	erase_button=sender.superview['buttonErase']
+	
 	if t in '0123456789':
 		if label.text == '':
 			label.text = t
+			erase_button.tint_color='red'
 		else:
 			# Append number:
 			label.text += t
@@ -63,22 +69,34 @@ def button_tapped(sender):
 		label.text = label.text[:-1]
 		if len(label.text) == 0:
 			label.text = ''
+			erase_button.tint_color='magenta'
 	elif t == 'DNF' or t == 'Stamp':
 		if len(label.text)>0:
-			DNF=True if t == 'DNF' else False
-			so_far+=1
-			update_results(so_far, label.text,DNF)
+			MSG='DNF' if t == 'DNF' else ''
+			if not label.text in so_far:
+				so_far.append(label.text)
+			update_results(len(so_far), label.text,MSG)
 			resultstxt.text=results
 			label.text=""
+			erase_button.tint_color='magenta'
 	elif t == 'Copy':
 		clipboard.set(results_csv)
 		console.hud_alert('Copied to clipboard')
-	elif t == 'Reset':
-		res=console.alert('Are you sure?', 'Clear Results','Proceed')
-		results=''
-		results_csv=''
-		so_far=0
-		resultstxt.text=results
+	elif t == 'Erase':
+		if len(label.text)==0:
+			res=console.alert('Are you sure?', 'Clear Results','Proceed')
+			results=''
+			resultstxt.text=results
+			results_csv=''
+			so_far=[]
+		else:
+			if label.text in so_far:
+				so_far.remove(label.text)
+				update_results(len(so_far),label.text,"ER")
+				resultstxt.text=results
+				label.text=''
+				erase_button.tint_color='magenta'
+	
 
 
 ########################
